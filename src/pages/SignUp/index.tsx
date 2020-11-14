e07b2d31efe7399c43fcc7b9fdc28ac3bb0419d8
MarcoCPinho <marcocpinho@hotmail.com>
2020-11-14 11:56:05 -0300

    três aulas
    - Isolando componentes (isolando e refatorando componentes)
    - Página de cadastro (criando pag, reaproveitando cod.)
    - Utilizando Unform (refatorando para lib Unform)

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
@@ -9,19 +9,21 @@ import { Container, Content, Background } from './styles';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
-const SignIn: React.FC = () => {
+const SignUp: React.FC = () => {
   function handleSubmit(data: object): void {
     console.log(data);
   }
 
   return (
     <Container>
+      <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
         <Form onSubmit={handleSubmit}>
-          <h1>Faça seu logon</h1>
+          <h1>Faça seu cadastro</h1>
 
+          <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
           <Input
             name="password"
@@ -30,18 +32,15 @@ const SignIn: React.FC = () => {
             placeholder="Senha"
           />
 
-          <Button type="submit">Entrar</Button>
-
-          <a href="forgot">Esqueci minha senha</a>
+          <Button type="submit">Cadastrar</Button>
         </Form>
         <a href="login">
-          <FiLogIn />
-          Criar conta
+          <FiArrowLeft />
+          Voltar para Logon
         </a>
       </Content>
-      <Background />
     </Container>
   );
 };
 
-export default SignIn;
+export default SignUp;
